b8855597325429468a6685d1163c0fbb06b8686c
miguel <miguel.martin@agsgroup.net>
2011-08-10 19:26:47 +0200

    instalacion limpia de spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impulska::Application.config.secret_token = 'b2a85fe183f1824b17f1e5f97ea218bebd24d7358fdff6dfa5dfa9238ff579463cefeea2dbec27f3147ff86dc3ac985cd640846d986f1b3aa17c45fdd0f1372c'
